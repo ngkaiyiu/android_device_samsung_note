0a0c61147f6f0ee4f7e0e12479e1c231c0db8005
Sebastian Mauer <sebastian@n-unity.de>
2011-11-28 19:33:08 +0100

    Clesaned up BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,17 +16,9 @@
 
 # This variable is set first, so it can be overridden
 # by BoardConfigVendor.mk
-BOARD_USES_LIBSECRIL_STUB := true
-
-# Set this up here so that BoardVendorConfig.mk can override it
-BOARD_USES_GENERIC_AUDIO := false
 USE_CAMERA_STUB := true
-
-BOARD_USE_AUDIO_LEGACY := true
-BOARD_USE_YAMAHAPLAYER := true
-
-# Use the non-open-source parts, if they're present
--include vendor/samsung/note/BoardConfigVendor.mk
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_USES_LIBSECRIL_STUB := true
 
 # CPU
 TARGET_CPU_ABI := armeabi-v7a
@@ -37,12 +29,20 @@ TARGET_ARCH_VARIANT_CPU := cortex-a9
 ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
+TARGET_BOARD_PLATFORM := smdkv310
+
+TARGET_BOOTLOADER_BOARD_NAME := GT-N7000
+TARGET_BOARD_INFO_FILE := device/samsung/note/board-info.txt
+TARGET_OTA_ASSERT_DEVICE := note,GT-N7000
+-include vendor/samsung/note/BoardConfigVendor.mk
 
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 TARGET_NO_KERNEL := false
+
 TARGET_PROVIDES_INIT := true
 TARGET_PROVIDES_INIT_TARGET_RC := true
+TARGET_RECOVERY_INITRC := device/samsung/note/recovery.rc
 
 BOARD_KERNEL_BASE := 0x40000000
 BOARD_NAND_PAGE_SIZE := 4096 -s 128
@@ -50,32 +50,7 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_CMDLINE := console=ttySAC2,115200 consoleblank=0
 TARGET_PREBUILT_KERNEL := device/samsung/note/kernel
 
-TARGET_BOARD_PLATFORM := smdkv310
-TARGET_BOOTLOADER_BOARD_NAME := GT-N7000
-TARGET_RECOVERY_INITRC := device/samsung/note/recovery.rc
-TARGET_PROVIDES_MEDIASERVER := true
-TARGET_BOARD_INFO_FILE := device/samsung/note/board-info.txt
-
-#BOARD_EGL_CFG := device/samsung/note/egl.cfg
-
-# Camera
-ifeq ($(USE_CAMERA_STUB),false)
-BOARD_CAMERA_LIBRARIES := libcamera
-endif
-
-# FM Radio
-BOARD_HAVE_FM_RADIO := true
-BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
-BOARD_FM_DEVICE := si4709
-
-#BOARD_USES_HGL := true
-#BOARD_USES_OVERLAY := true
-USE_OPENGL_RENDERER := true
-
-# Releasetools
-TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/c1-common/releasetools/c1_ota_from_target_files
-TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/c1-common/releasetools/c1_img_from_target_files
-
+# Filesystem
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_USES_MMCUTILS := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
@@ -83,10 +58,33 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 893386752
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
 BOARD_FLASH_BLOCK_SIZE := 4096
 
-# NFC, let's assume we have it
-BOARD_HAVE_NFC := true
+# Releasetools
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/c1-common/releasetools/c1_ota_from_target_files
+TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/c1-common/releasetools/c1_img_from_target_files
 
-# Wifi related defines
+# Graphics (Mali 400)
+BOARD_EGL_CFG := device/samsung/note/configs/egl.cfg
+USE_OPENGL_RENDERER := true
+BOARD_USE_LEGACY_TOUCHSCREEN := true
+
+# Camera
+ifeq ($(USE_CAMERA_STUB),false)
+BOARD_CAMERA_LIBRARIES := libcamera
+endif
+
+# Audio
+BOARD_USES_AUDIO_LEGACY := true
+BOARD_USE_YAMAHAPLAYER := true
+
+# FM Radio
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+BOARD_FM_DEVICE := si4709
+
+# GPS
+BOARD_USES_GPSWRAPPER := true
+
+# WiFi (BCM4330)
 BOARD_WPA_SUPPLICANT_DRIVER := WEXT
 WPA_SUPPLICANT_VERSION      := VER_0_8_X
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
@@ -98,17 +96,14 @@ BOARD_WLAN_DEVICE_REV       := bcm4330_b1
 WIFI_DRIVER_MODULE_PATH     := "/lib/modules/dhd.ko"
 WIFI_DRIVER_FW_PATH_STA     := "/system/vendor/firmware/bcm4330_sta.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/system/vendor/firmware/bcm4330_aps.bin"
-WIFI_DRIVER_FW_PATH_P2P     := "/system/vendor/firmware/fw_bcmdhd_p2p.bin"
 WIFI_DRIVER_MODULE_NAME     :=  "dhd"
 WIFI_DRIVER_MODULE_ARG      :=  "firmware_path=/system/vendor/firmware/bcm4330_sta.bin nvram_path=/system/etc/nvram_net.txt"
 WIFI_BAND                   := 802_11_ABG
 
+# Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_UMS_LUNFILE := /sys/class/android_usb/android0/f_mass_storage/lun/file
-
-# gps
-BOARD_USES_GPSWRAPPER := true
 
 # Vold
 BOARD_VOLD_MAX_PARTITIONS := 12
@@ -121,17 +116,12 @@ TARGET_USES_OLD_LIBSENSORS_HAL := true
 BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/samsung/c1-common/vibrator/tspdrv.c
 
 # Recovery
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_MISC_PARTITION := true 
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/note/recovery/recovery_keys.c
-BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/note/recovery/graphics.c
+BOARD_CUSTOM_GRAPHICS := ../../device/samsung/note/recovery/graphics.c
 
 BOARD_CUSTOM_BOOTIMG_MK := device/samsung/c1-common/shbootimg.mk
 
-# assert
-TARGET_OTA_ASSERT_DEVICE := note,GT-N7000
-
-# Include aries specific stuff
+# Include C1 specific stuff
 -include device/samsung/c1-common/Android.mk
